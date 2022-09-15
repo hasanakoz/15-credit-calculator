d7705c7dd27ef76ce924c5a9c5812bd9e55aef05
hasan <hasanakoz1986@gmail.com>
2022-09-15 20:03:46 +0300

    js variables defined

--- code.js
+++ code.js
@@ -1,10 +1,10 @@
-// const loanType = document.querySelector("#loan-type");
-// const loanTerm = document.querySelector("#loan-term");
-// const loanAmount = document.querySelector("#loan-amount");
-// const calculate = document.querySelector(".submit");
-// const housingLoan = 1.29;
-// const carLoan = 1.79;
-// const personalFinanceLoan = 1.99;
+const loanType = document.querySelector("#loan-type");
+const loanTerm = document.querySelector("#loan-term");
+const loanAmount = document.querySelector("#loan-amount");
+const calculate = document.querySelector(".submit");
+const housingLoan = 1.29;
+const carLoan = 1.79;
+const personalFinanceLoan = 1.99;
 
 // calculate.addEventListener("click", ()=>{
 //     if
