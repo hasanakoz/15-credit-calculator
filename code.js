668f0c6531dfcf5f24bca3bbd58ffa7dabd4c1ea
hasan <hasanakoz1986@gmail.com>
2022-09-16 11:10:45 +0300

    trial

--- code.js
+++ code.js
@@ -1,22 +1,22 @@
 let calculateButton = document.querySelector(".submit");
-let loan = document.querySelector("#loan-type");
-let loanType = loan.options[loan.selectedIndex].value;
-let loanAmount = Number(document.getElementById("loan-amount").value);
-let loanTerm = Number(document.getElementById("loan-term").value);
-let result = document.querySelector(".result");
-let x = 0;
-if (loanType == "housing") {
-  x = 1.29 / 100;
-} else if (loanType == "car") {
-  x = 1.79 / 100;
-} else if (loanType == "personal") {
-  x = 1.99 / 100;
-}
-
-let installment =
-  loanAmount * ((x * (1 + x) ** loanTerm) / ((1 + x) ** loanTerm - 1));
 
 calculateButton.addEventListener("click", () => {
-  (result.textContent = "taksit: "), installment;
+  let loan = document.querySelector("#loan-type");
+  let loanType = loan.options[loan.selectedIndex].value;
+  let loanAmount = Number(document.getElementById("loan-amount").value);
+  let loanTerm = Number(document.getElementById("loan-term").value);
+  let result = document.querySelector(".result");
+  let x = 0;
+  if (loanType == "housing") {
+    x = 1.29 / 100;
+  } else if (loanType == "car") {
+    x = 1.79 / 100;
+  } else if (loanType == "personal") {
+    x = 1.99 / 100;
+  }
+
+  let installment =
+    loanAmount * ((x * (1 + x) ** loanTerm) / ((1 + x) ** loanTerm - 1));
+  result.textContent = `taksit: ${installment}`;
   console.log(installment, x, loanType, loanAmount, loanTerm);
 });
