4de569e18fe284f9fa98b90de5abff9580b414b7
hasan <hasanakoz1986@gmail.com>
2022-09-16 10:46:48 +0300

    js dom structure

--- code.js
+++ code.js
@@ -1,11 +1,22 @@
-const loanType = document.querySelector("#loan-type");
-const loanTerm = document.querySelector("#loan-term");
-const loanAmount = document.querySelector("#loan-amount");
-const calculate = document.querySelector(".submit");
-const housingLoan = 1.29;
-const carLoan = 1.79;
-const personalFinanceLoan = 1.99;
+let calculateButton = document.querySelector(".submit");
+let loan = document.querySelector("#loan-type");
+let loanType = loan.options[loan.selectedIndex].value;
+let loanAmount = Number(document.getElementById("loan-amount").value);
+let loanTerm = Number(document.getElementById("loan-term").value);
+let result = document.querySelector(".result");
+let x = 0;
+if (loanType == "housing") {
+  x = 1.29 / 100;
+} else if (loanType == "car") {
+  x = 1.79 / 100;
+} else if (loanType == "personal") {
+  x = 1.99 / 100;
+}
 
-// calculate.addEventListener("click", ()=>{
-//     if
-// })
+let installment =
+  loanAmount * ((x * (1 + x) ** loanTerm) / ((1 + x) ** loanTerm - 1));
+
+calculateButton.addEventListener("click", () => {
+  (result.textContent = "taksit: "), installment;
+  console.log(installment, x, loanType, loanAmount, loanTerm);
+});
